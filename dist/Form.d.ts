dd3f33147d99e75262811662314c05ff27d4295b
daniel <danieljee123@gmail.com>
2020-02-19 15:41:48 +1000

    little type change

--- dist/Form.d.ts
+++ dist/Form.d.ts
@@ -2,7 +2,6 @@ declare type FIELD_TYPE_TEXT = 'text';
 declare type FIELD_TYPE_NUMBER = 'number';
 declare type FIELD_TYPE_HIDDEN = 'hidden';
 declare type FIELD_TYPE_FILE = 'file';
-declare type FIELD_TYPES = FIELD_TYPE_TEXT | FIELD_TYPE_NUMBER | FIELD_TYPE_HIDDEN | FIELD_TYPE_FILE;
 declare type MEDIA_TYPE_OCTET = 'application/octet-stream';
 declare type MEDIA_TYPE_JSON = 'application/json';
 declare type MEDIA_TYPE_HAL = 'application/hal+json';
@@ -17,7 +16,7 @@ declare type HTTP_PATCH = 'PATCH' | 'patch';
 declare type HTTP_METHODS = HTTP_GET | HTTP_POST | HTTP_PUT | HTTP_DELETE | HTTP_PATCH;
 interface IFieldObject {
     name: string;
-    type: FIELD_TYPES;
+    type: FIELD_TYPE_TEXT | FIELD_TYPE_NUMBER | FIELD_TYPE_HIDDEN | FIELD_TYPE_FILE;
     required?: boolean;
     default?: string | number;
     regex?: RegExp;
